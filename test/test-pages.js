387734feb982008d2564866e874d025c27cc66e7
Joey Mullica <joeymullica522@gmail.com>
2020-09-09 09:29:24 -0400

    Added tests Unit tests checking positive status codes added. Addition of Husky to ensure tests pass before commits fire.

---
+++ test/test-pages.js
@@ -0,0 +1,31 @@
+const expect = require('chai').expect;
+const request = require('request');
+
+let url = 'http://localhost:3000';
+
+describe('Page status and content', function() {
+    describe('Main page', function() {
+        it('Should return 200', function(done) {
+            request(url, function(error, response, body) {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+    describe('First Article page', function() {
+        it('Should return 200', function(done) {
+            request((url + '/articles/1'), function(error, response, body) {
+                expect(response.statusCode).to.equal(200);
+                done();
+            });
+        });
+    });
+    describe('Everything else', function() {
+        it('should 404', function(done) {
+            request((url + '/foo/bar'), function(error, response, body) {
+                expect(response.statusCode).to.equal(404);
+                done();
+            });
+        });
+    });
+});
